c16c378ae3366bbf27856ec448997c36f8492b05
ray <raymondovandijk@gmail.com>
2020-09-17 02:45:14 +0200

    made some mistakes in gatsby-config.js, and added a post

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'pages',
-        path: '${__dirname}/scr/pages'
+        path: '$(__dirname)/src/pages/'
 
       },
     },
@@ -12,7 +12,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'post',
-        path: '${__dirname}/scr/post'
+        path: '$(__dirname)/src/post/'
 
       },
     },
@@ -20,7 +20,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: '${__dirname}/scr/images'
+        path: '$(__dirname)/src/images/'
 
       },
     },
